61c872881d9ce73b3a3cf722f9591fd9c76e139d
M J <mohamedmujtaba0210@gmail.com>
2023-04-19 00:13:34 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import { CommandDialogDemo } from "@/components/cmd";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Switch } from "@/components/ui/switch";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <CommandDialogDemo />
+      <Button>mohamed</Button>
+      <Badge variant="secondary">Badge</Badge>
+      <Switch />
+      <>
+        <RadioGroup defaultValue="comfortable">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="default" id="r1" />
+            <Label htmlFor="r1">Default</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="comfortable" id="r2" />
+            <Label htmlFor="r2">Comfortable</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="compact" id="r3" />
+            <Label htmlFor="r3">Compact</Label>
+          </div>
+        </RadioGroup>
+      </>
+    </div>
+  );
+};
+
+export default index;
